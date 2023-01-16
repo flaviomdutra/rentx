9ed4173d3338fd600df15d77da3486e223e6d8ac
Flávio Dutra <flaviomdutra@outlook.com>
2023-01-16 20:20:06 -0300

    feat: add theme

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';
-import { AppProvider } from './src/hooks';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+// import { AppProvider } from './src/hooks';
 
 import {
   useFonts,
   Inter_400Regular,
-  Inter_500Medium
-} from '@expo-google-fonts/inter';
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 import {
   Archivo_400Regular,
   Archivo_500Medium,
-  Archivo_600SemiBold
-} from '@expo-google-fonts/archivo';
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
 
-import { Routes } from './src/routes';
+// import { Routes } from './src/routes';
 
-import theme from './src/styles/theme';
+import theme from "./src/styles/theme";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,18 +25,18 @@ export default function App() {
     Inter_500Medium,
     Archivo_400Regular,
     Archivo_500Medium,
-    Archivo_600SemiBold
+    Archivo_600SemiBold,
   });
 
-  if(!fontsLoaded){
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
-    <ThemeProvider theme={theme}>    
-      <AppProvider>
-        <Routes />      
-      </AppProvider>  
+    <ThemeProvider theme={theme}>
+      {/* <AppProvider> */}
+      <Home />
+      {/* </AppProvider> */}
     </ThemeProvider>
-  )
+  );
 }
